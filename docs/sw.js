cfbf9d4457e1404cca2ad2a9ed374a8aa2b7f388
Selapian <christianjosiahperrone@gmail.com>
2026-07-07 10:55:04 -0400

    Create sw.js

---
+++ docs/sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'quantum-gong-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './gong.mp3' // Add your audio file name here if you have one!
+];
+
+// Install the service worker and cache assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Activate and clean up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch assets from cache first, then network fallback
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
